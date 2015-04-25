371de251577a8d86b8f7fbbe27dceb5009962909
Stackdevel <stackdevel@gmail.com>
2015-04-25 16:57:01 +0300

    chart.js responsive

--- js/custom.js
+++ js/custom.js
@@ -53,7 +53,7 @@ Chart.defaults.global = {
     scaleFontColor: "#5A616D",
 
     // Boolean - whether or not the chart should be responsive and resize when the browser does.
-    // responsive: true,
+    responsive: true,
 
     // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
     maintainAspectRatio: true,
